5e93115f11d90d0d42234283a55b2866385509dc
SwagDeNoli <chris.samuels.va@gmail.com>
2019-09-20 22:05:49 -0400

    Added a base Engine class to handle engine functions, instead of handling it all in main

---
+++ src/Engine.cpp
@@ -0,0 +1,5 @@
+//
+// Created by CHRIS on 9/20/2019.
+//
+
+#include "Engine.h"
---
+++ src/Engine.h
@@ -0,0 +1,15 @@
+//
+// Created by CHRIS on 9/20/2019.
+//
+
+#ifndef PONG_ENGINE_H
+#define PONG_ENGINE_H
+
+
+class Engine
+{
+
+};
+
+
+#endif //PONG_ENGINE_H
